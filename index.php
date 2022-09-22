a4edca4300af3fd8bb15a8f8b725b61dc598f70c
DavideLopez <davidelopez7@gmail.com>
2022-09-22 17:23:07 +0200

    inserito file php e css

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>GOOGLE FAQ</title>
+</head>
+<body>
+
+  <header>
+      <a href="#" class="google-logo"><img src="./img/Google_2015_logo.svg" alt="logo"></a>
+      <h3>Privacy e Termini</h3>
+
+      
+  </header>
+    
+</body>
+</html>
